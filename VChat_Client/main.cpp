7f5cf5b70c9b6cfe35fdd0406d15c1ffa415a0df
Vadym Retz <viar99criminal@gmail.com>
2017-11-30 21:58:32 +0200

    Version number updated to 1.1

--- VChat_Client/main.cpp
+++ VChat_Client/main.cpp
@@ -135,7 +135,7 @@ void main() {
 		return;
 	}
 	cout << ">--------------------------------------<\n";
-	cout << "VChat 1.0\n";
+	cout << "VChat 1.1\n";
 	cout << "\"A meaningful silence is always better than meaningless words\" - Anonymous\n\n";
 	cout << "Welcome, stranger\n";
 	cout << "Send an empty message to stop the server\n";
